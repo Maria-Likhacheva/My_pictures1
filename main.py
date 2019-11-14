93cd69f8b016dade9dbdfbd2f1114610e4cb4dfc
irina-makarova <56422684+irina-makarova@users.noreply.github.com>
2019-11-15 04:12:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -75,9 +75,25 @@ def main():
     pass
 
     # TODO: (Irina) Figure 2
+    triangle(250, -75, 50, 'orangered', 270)
+    square(200, -25, 50, 'darkorange', 0)
+    square(150, -25, 50, 'darkorange', 0)
+    triangle(150, -25, 50, 'orangered', 90)
+    square(150, -25, 50, 'gold', 150)
+    triangle(150, -25, 50, 'gold', 240)
+    square(125, 18.3, 50, 'gold', 270)
+    triangle(175, 68.3, 70, 'orangered', 180)
     pass
 
     # TODO: (Irina) Figure 3
+    square(-250, -200, 50, 'lime', 0)
+    square(-250, -150, 50, 'aqua', 0)
+    square(-200, -200, 50, 'aqua', 0)
+    square(-200, -150, 50, 'lime', 0)
+    triangle(-150, -150, 100, 'yellow', 30)
+    triangle(-250, -200, 100, 'dodgerblue', 150)
+    triangle(-190, -150, 40, 'mediumspringgreen', 300)
+    triangle(-190, -250, 40, 'mediumspringgreen', 0)
     pass
 
 main()
