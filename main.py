4f271ff3c59660d395956d99122009bf871c9bac
Maria-Likhacheva <55570096+Maria-Likhacheva@users.noreply.github.com>
2019-11-15 04:05:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main():
     square(-250, 50, 50, 'slateblue', 0)
     square(-200, 0, 50, 'slateblue', 0)
     square(-200, 50, 50, 'violet', 0)
-    square(-150, 90, 40, 'orange', 0)
+    square(-150, 90, 45, 'orange', 0)
     triangle(-200, 50, 40, 'orange', 225)
     triangle(-150, 0, 40, 'orange', 345)
     triangle(-200, -50, 40, 'orange', 75)
@@ -64,6 +64,14 @@ def main():
     
     pass
     # TODO: (Maria) Figure 3
+    square(-250, -200, 50, 'lime', 0)
+    square(-250, -150, 50, 'aqua', 0)
+    square(-200, -200, 50, 'aqua', 0)
+    square(-200, -150, 50, 'lime', 0)
+    triangle(-150, -150, 100, 'yellow', 30)
+    triangle(-250, -200, 100, 'dodgerblue', 150)
+    triangle(-190, -150, 40, 'mediumspringgreen', 300)
+    triangle(-190, -250, 40, 'mediumspringgreen', 0)
     pass
 
     # TODO: (Irina) Figure 2
