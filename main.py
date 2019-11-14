97ff4b242f757903768867efff47e63da17e92cc
Maria-Likhacheva <55570096+Maria-Likhacheva@users.noreply.github.com>
2019-11-14 23:39:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ import turtle
 
 def sguare(x, y, size, colour, angle)
     # TODO: (Maria)
+    turtle.pencolor(colour)
+    turtle.fillcolor(colour)
+    turtle.up()
+    turtle.down()
+    turtlr.goto(x, y)
+    turtle.right(angle)
+    turtle.begin_fill()
+    turtle.forward(size)
+    turtle.right(90)
+    turtle.forwardh(size)
+    turtle.right(90)
+    turtle.forwardh(size)
+    turtle.right(90)
+    turtle.forwardh(size)
+    turtle.end_fill()
+    turtle(up)
+    turtle(home) 
     pass
 
 def triangle(x, y, size, colour, angle)
     # TODO: (Irina)
-    turtle.pencolor(colour)
-    turtle.fillcolor(colour)
-    turtle.up()
-    turtle.goto(x, y)
-    turtle.down()
-    turtle.right(angle)
-    turtle.begin_fill()
-    turtle.forward(size)
-    turtle.right(120)
-    turtle.forward(size)
-    turtle.right(120)
-    turtle.forward(size)
-    turtle.end_fill()
-    turtle.up()
-    turtle.home
     pass
-
 def main():
     # TODO: (Maria) Figure 1
     pass
