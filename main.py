c8710ac4f3b71b0ef251d7043b44936ce798338d
irina-makarova <56422684+irina-makarova@users.noreply.github.com>
2019-11-15 04:14:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -61,17 +61,6 @@ def main():
     triangle(-150, 0, 40, 'orange', 345)
     triangle(-200, -50, 40, 'orange', 75)
     triangle(-250, 0, 40, 'orange', 135)
-    
-    pass
-    # TODO: (Maria) Figure 3
-    square(-250, -200, 50, 'lime', 0)
-    square(-250, -150, 50, 'aqua', 0)
-    square(-200, -200, 50, 'aqua', 0)
-    square(-200, -150, 50, 'lime', 0)
-    triangle(-150, -150, 100, 'yellow', 30)
-    triangle(-250, -200, 100, 'dodgerblue', 150)
-    triangle(-190, -150, 40, 'mediumspringgreen', 300)
-    triangle(-190, -250, 40, 'mediumspringgreen', 0)
     pass
 
     # TODO: (Irina) Figure 2
@@ -83,6 +72,17 @@ def main():
     triangle(150, -25, 50, 'gold', 240)
     square(125, 18.3, 50, 'gold', 270)
     triangle(175, 68.3, 70, 'orangered', 180)
+    pass
+
+    # TODO: (Maria) Figure 3
+    square(-250, -200, 50, 'lime', 0)
+    square(-250, -150, 50, 'aqua', 0)
+    square(-200, -200, 50, 'aqua', 0)
+    square(-200, -150, 50, 'lime', 0)
+    triangle(-150, -150, 100, 'yellow', 30)
+    triangle(-250, -200, 100, 'dodgerblue', 150)
+    triangle(-190, -150, 40, 'mediumspringgreen', 300)
+    triangle(-190, -250, 40, 'mediumspringgreen', 0)
     pass
 
     # TODO: (Irina) Figure 3
