d3a718b0d53a4765ffccbc104cff99b336f43e34
Maria-Likhacheva <55570096+Maria-Likhacheva@users.noreply.github.com>
2019-11-14 22:59:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import turtle
 
+def sguare(x, y, size, colour, angle)
+    # TODO: (Maria)
+    pass
+
+def triangle(x, y, size, colour, angle)
+    # TODO: (Irina)
+    pass
+def main():
+    # TODO: (Maria) Figure 1
+    pass
+    # TODO: (Maria) Figure 2
+    pass
+    # TODO: (Irina) Figure 1
+    pass
+    # TODO: (Irina) Figure 2
+    pass
