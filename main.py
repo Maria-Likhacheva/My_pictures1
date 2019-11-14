8c3877b1469c2708283932aea0740a4817a2d752
irina-makarova <56422684+irina-makarova@users.noreply.github.com>
2019-11-14 23:38:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ def sguare(x, y, size, colour, angle)
 
 def triangle(x, y, size, colour, angle)
     # TODO: (Irina)
+    turtle.pencolor(colour)
+    turtle.fillcolor(colour)
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.right(angle)
+    turtle.begin_fill()
+    turtle.forward(size)
+    turtle.right(120)
+    turtle.forward(size)
+    turtle.right(120)
+    turtle.forward(size)
+    turtle.end_fill()
+    turtle.up()
+    turtle.home
     pass
+
 def main():
     # TODO: (Maria) Figure 1
     pass
